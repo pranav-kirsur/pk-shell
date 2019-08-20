e5dec20f24fff1d97692aab2b2f61b94fa5fa99b
pranav-kirsur <pranav.kirsur@gmail.com>
2019-08-21 01:37:17 +0530

    Added #include guard

--- init.h
+++ init.h
@@ -1,2 +1,8 @@
+#IFNDEF __INIT_H
+#DEFINE __INIT_H
+
+
 /* Performs initialisation for the shell such as setting the home directory */
 void init();
+
+#ENDIF
--- prompt.h
+++ prompt.h
@@ -1,2 +1,8 @@
+#IFNDEF __PROMPT_H
+#DEFINE __PROMPT_H
+
+
 /* Prints prompt before every line */
 void prompt();
+
+#ENDIF
