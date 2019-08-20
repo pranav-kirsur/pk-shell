f3dbfcac21aa4f12ad3452061273db0f019579fc
pranav-kirsur <pranav.kirsur@gmail.com>
2019-08-21 03:35:36 +0530

    Fixed prompt wrt relative path from home

--- prompt.c
+++ prompt.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "prompt.h"
 #include "shell.h"
 
@@ -13,6 +14,17 @@ void prompt()
     char current_directory[4096];
     getcwd(current_directory, 4096);
 
+    //check if current directory is child of ~
+    int size_of_home = strlen(HOME);
+    if(strncmp(HOME,current_directory,size_of_home)==0)
+    {
+        char relative_current_directory[4096];
+        relative_current_directory[0]='~';
+        relative_current_directory[1]='\0';
+        strcat(relative_current_directory,current_directory+size_of_home);
+        strcpy(current_directory,relative_current_directory);   
+    }
+
     //print prompt
-    printf("\n>%s@%s:%s",user,hostname,current_directory);
+    printf("\n>%s@%s:%s $",user,hostname,current_directory);
 }
