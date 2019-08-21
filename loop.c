13edf84d8a2341847528879299716c95b93a7a22
pranav-kirsur <pranav.kirsur@gmail.com>
2019-08-21 16:47:37 +0530

    Added readline

--- loop.c
+++ loop.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "loop.h"
 #include "shell.h"
 #include "prompt.h"
@@ -5,12 +6,15 @@
 void loop()
 {
     char *line;
+    int linesize = 0;
     char **args;
     int status;
 
     do
     {
         prompt();
+        getline(&line, &linesize,stdin);
+        printf("%s",line);
         status = 1;
     } while (status);
 }
