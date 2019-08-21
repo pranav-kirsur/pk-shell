e18411c19c43a76c3e58dc7c4cf0547080346fb3
pranav-kirsur <pranav.kirsur@gmail.com>
2019-08-21 18:02:28 +0530

    Parsing in loop

--- loop.c
+++ loop.c
@@ -10,13 +10,26 @@ void loop()
     int linesize = 0;
     char **args;
     int status;
+    char* delim = " \t\n\r\f";
 
     do
     {
         prompt();
         getline(&line, &linesize,stdin);
         char** commands = parse(line, ";");
+        for(int i=0;i<sizeof(commands);i++)
+        {
+            if(commands[i]==NULL)
+            {
+                break;
+            }
+            //printf("%s\n",commands[i]);
 
+            //parse by whitespace characters
+            args = parse(commands[i], delim);
+           
+
+        }
         //printf("%s",line);
         status = 1;
     } while (status);
